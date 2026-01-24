d95def880209943fa17c6b23a927cd9cebff4955
Zenx007 <zenxpokas@gmail.com>
2026-01-24 19:31:01 -0300

    Feat SeatController

---
+++ src/API/Controllers/Seat.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Get, Res, Req, Query } from "@nestjs/common";
+import { ApiTags, ApiOperation } from "@nestjs/swagger";
+import { Response, Request } from "express";
+import { SeatVO } from "../../Communication/ViewObjects/Seat/SeatVO";
+import { ISeatService } from "../../Core/ServicesInterface/ISeatService.interface";
+import { ConstantsMessagesSeat } from "../../Helpers/ConstantsMessages/ConstantsMessages";
+import { List } from "../../Helpers/CustomObjects/List.Interface";
+import { StatusCode, StatusCodes } from "../../Helpers/StatusCode/StatusCode";
+import { ApiResponse } from "../../Helpers/CustomObjects/ApiResponse.interface";
+
+@ApiTags('Seats') 
+@Controller("Seat")
+export class SeatController {
+    constructor(
+        private readonly _seatService: ISeatService,
+    ) { }
+
+    @ApiOperation({ summary: 'GetAvailable - Lista apenas assentos disponiveis de uma sessão' })
+    @Get('GetAvailable')
+    async GetAvailableAsync(
+        @Res() res: Response,
+        @Req() req: Request,
+        @Query('sessionId') sessionId: string
+    ) {
+        const response = new ApiResponse<List<SeatVO>>();
+        try {
+            const result = await this._seatService.GetAvailableBySession(sessionId);
+
+            if (result.isFailed) {
+                response.success = false;
+                response.message = ConstantsMessagesSeat.ErrorFindAll;
+                return StatusCode(res, StatusCodes.STATUS_400_BAD_REQUEST, response);
+            }
+
+            response.success = true;
+            response.object = result.value;
+
+            return StatusCode(res, StatusCodes.STATUS_200_OK, response);
+
+        } catch (error) {
+            response.success = false;
+            response.message = ConstantsMessagesSeat.ErrorFindAll;
+            return StatusCode(res, StatusCodes.STATUS_500_INTERNAL_SERVER_ERROR, response);
+        }
+    }
+}
--- src/API/Extensions/AddController.ts
+++ src/API/Extensions/AddController.ts
@@ -1,7 +1,9 @@
 import { ReservationController } from "../Controllers/Reservation.controller";
+import { SeatController } from "../Controllers/Seat.controller";
 import { SessionController } from "../Controllers/Session.controller";
 
 export const AddControllers = [
 SessionController,
 ReservationController,
+SeatController,
 ];
